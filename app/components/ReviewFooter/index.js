77f1708c7a9f881e867186a79481fffe79417b1e
DJTB <duncan.bay@gmail.com>
2016-11-14 16:07:45 +0700

    copy image assets from KW

--- app/components/ReviewFooter/index.js
+++ app/components/ReviewFooter/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+
+// TODO: responsive, art directed png only images instead since the svg is huge
 import bg from 'shared/assets/img/backgrounds/reviews-800.png';
 import bgMd from 'shared/assets/img/backgrounds/reviews-1200.png';
 import bgXl from 'shared/assets/img/backgrounds/reviews.svg';
@@ -15,19 +17,19 @@ const BackgroundImg = styled.div`
   width: 100%;
   height: 100%;
   opacity: .9;
-  background-image: url("../../shared/assets/img/backgrounds/reviews-800.png");
+  background-image: url(${bg});
   background-repeat: no-repeat;
   background-position: bottom right;
   background-size: auto;
 
   @media(min-width: 600px) {
     background-size: contain;
-    background-image: url("../../shared/assets/img/backgrounds/reviews-1200.png");
+    background-image: url(${bgMd});
   }
 
   @media(min-width: 1200px) {
     background-size: cover;
-    background-image: url("../../shared/assets/img/backgrounds/reviews.svg");
+    background-image: url(${bgXl});
   }
 `;
 
