fa031e7ec9963531c08a7f44ada76c9a4f821d1e
DJTB <duncan.bay@gmail.com>
2016-11-29 10:20:16 +0700

    modify styled-components media function for better min/max ranges

--- app/shared/styles/media.js
+++ app/shared/styles/media.js
@@ -2,21 +2,26 @@ import { css } from 'styled-components';
 
 // these sizes are arbitrary and you can set them to whatever you wish
 export const sizes = {
-  xs: 320,
-  sm: 480,
-  md: 768,
-  lg: 1024,
-  xl: 1280,
+  xs: 400,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1600,
   xxl: 2000,
 };
 
 // iterate through the sizes and create a media template
 export const media = (direction) => Object.keys(sizes).reduce((accumulator, label) => {
   const acc = accumulator;
-  acc[label] = (...args) => css`
-    @media (${direction}-width: ${sizes[label]}px) {
-      ${css(...args)}
-    }
-  `;
+  acc[label] = (...args) => {
+    let size = sizes[label];
+    // to ensure (max-width: 599px) versus (min-width: 600px) [the next size up]
+    if (direction === 'max') size -= 1;
+
+    return css`
+      @media (${direction}-width: ${size}px) {
+        ${css(...args)}
+      }`;
+  };
   return acc;
 }, {});
