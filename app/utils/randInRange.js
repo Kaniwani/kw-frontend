abed928a54775c3a252416891d71d73aa0ef71b5
DJTB <duncan.bay@gmail.com>
2016-12-06 18:30:17 +0700

    add tests for global utils

--- app/utils/randInRange.js
+++ app/utils/randInRange.js
@@ -1,9 +1,9 @@
 /**
  * Returns random number between min and max inclusive
- * @param  {number} min starting number
- * @param  {number} max ending number
- * @return {number}
+ * @param  {Number} [min=0] starting number
+ * @param  {Number} [max=100] ending number
+ * @return {Number} integer between min and max
  */
-export default function randInRange(min, max) {
+export default function randInRange(min = 0, max = 100) {
   return Math.floor((Math.random() * ((max - min) + 1)) + min);
 }
