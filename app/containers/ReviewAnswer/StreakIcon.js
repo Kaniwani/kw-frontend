15225e72822634af13da31d528d9951b4ed2c0e0
DJTB <duncan.bay@gmail.com>
2016-12-12 13:16:06 +0700

    remove fixme

--- app/containers/ReviewAnswer/StreakIcon.js
+++ app/containers/ReviewAnswer/StreakIcon.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import getSrsRankName from 'utils/getSrsRankName';
 import Icon from 'components/Icon';
 
-// FIXME: might just need to add another wrapper before this since the Icon wrapper is pos: absolute already which we're messing up here. Or rethink styling since the old icon (scss/kw) never had a wrapper.
 const StyledIcon = styled(Icon)`
   display: block;
   position: absolute;
