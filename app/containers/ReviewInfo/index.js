b823cae59b84873820ada57f209408138a629fe6
DJTB <duncan.bay@gmail.com>
2016-12-20 18:17:01 +0700

    --wip-- [skip ci]

--- app/containers/ReviewInfo/index.js
+++ app/containers/ReviewInfo/index.js
@@ -30,50 +30,39 @@ export class ReviewInfo extends React.Component { // eslint-disable-line react/p
     const { readings, isInfoVisible, isAnswerCorrect } = this.props;
 
     const testComp = ({ item }) => (
-      <Row>
-        <Column md={3}>
-          <h5>Characters</h5>
-          <Entry item={item.get('character')} />
-        </Column>
-        <Column md={3}>
-          <h5>Kana</h5>
-          <Entry item={item.get('kana')} />
-        </Column>
-        <Column md={3}>
-          <h5>Parts of Speech</h5>
-          <List items={item.get('tags')} component={Entry} />
-        </Column>
-        <Column md={3}>
-          <h5>Sentence</h5>
-          <Entry lang="ja" item={item.get('sentence_ja')} />
-          <Entry item={item.get('sentence_en')} />
-        </Column>
-        <Column md={3}>
-          <h5>Tags</h5>
-          <Entry item={item.get('jlpt')} />
-          <Entry item={item.get('common') && 'Common'} />
-        </Column>
-      </Row>
+      <li>
+        <Row>
+          <Column md={3}>
+            <h5>Characters</h5>
+            <Entry item={item.get('character')} />
+          </Column>
+          <Column md={3}>
+            <h5>Kana</h5>
+            <Entry item={item.get('kana')} />
+          </Column>
+          <Column md={3}>
+            <h5>Parts of Speech</h5>
+            <List items={item.get('tags')} component={Entry} />
+          </Column>
+          <Column md={3}>
+            <h5>Sentence</h5>
+            <Entry lang="ja" item={item.get('sentence_ja')} />
+            <Entry item={item.get('sentence_en')} />
+          </Column>
+          <Column md={3}>
+            <h5>Tags</h5>
+            <Entry item={item.get('jlpt')} />
+            <Entry item={item.get('common') && 'Common'} />
+          </Column>
+        </Row>
+      </li>
     );
 
     return (
       <Wrapper>
         {isInfoVisible && (
         <InfoWrapper>
-          {/* readings: [
-            {
-              character: 'お誕生日おめでとう',
-              kana: 'おたんじょうびおめでとう',
-              level: 17,
-              tags: [
-                'Intransitive verb', 'Godan verb',
-              ],
-              sentence_en: 'She smoothly and elegantly poured the water into the glass.',
-              sentence_ja: '滞りのない優雅な仕草でグラスに水を注ぎ込んだ',
-              jlpt: 'JLPT N1',
-              common: false,
-            },
-          ], */}
+          {/* // TODO: SectionList instead */}
           <List items={readings} component={testComp} />
         </InfoWrapper>
         )}
