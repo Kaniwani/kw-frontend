7395c4a4e573aef78856ff176e744a73f6d92eb0
DJTB <duncan.bay@gmail.com>
2017-01-02 23:40:56 +0700

    feat(critical items): change minimum attempts to 3, matching backend

--- app/containers/ReviewSummary/utils/isCritical.js
+++ app/containers/ReviewSummary/utils/isCritical.js
@@ -1,12 +1,11 @@
-
 /**
  * Determines if review item is critical by comparing correct answers against total times answered
  * @param  {object} review Item to check
  * @param  {number} [threshold=0.75] Correctness ratio below which item is considered critical
- * @param  {number} [minimum=2] Number of times item must have been answered previously
+ * @param  {number} [minimum=4] Number of times item must have been answered previously
  * @return {boolean} True if item is below critical threshold
  */
-function isCritical(review, threshold = 0.75, minimum = 2) { // eslint-disable-line no-unused-vars
+function isCritical(review, threshold = 0.75, minimum = 4) { // eslint-disable-line no-unused-vars
   const correctTotal = review.getIn(['history', 'correct']) + review.getIn(['session', 'correct']);
   const answeredTotal = review.getIn(['history', 'incorrect']) + review.getIn(['session', 'incorrect']) + correctTotal;
   const correctRatio = correctTotal / answeredTotal;
