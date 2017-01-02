da2562b8c826fe4034ee6e35fc39d15adaa75d37
DJTB <duncan.bay@gmail.com>
2017-01-02 14:50:04 +0700

    fix(add synonym form): fix tabindex typo, avoid passing unused styling prop to anchor element

--- app/components/JishoSearchLink/index.js
+++ app/components/JishoSearchLink/index.js
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Icon from 'components/Icon';
 import A from 'components/A';
 import { ghost } from 'shared/styles/utils';
 
-const StyledAnchor = styled(A)`
+const AnchorStyles = css`
   display: block;
   padding: .2em .4em;
   transform: scale(.95);
@@ -16,11 +16,13 @@ const StyledAnchor = styled(A)`
   ${(props) => props.visuallyHidden ? ghost : ''}
 `;
 
+const StyledAnchor = styled(({ visuallyHidden, ...props }) => <A {...props} />)`${AnchorStyles}`; // eslint-disable-line no-unused-vars
+
 function JishoSearchLink({ keyword, visuallyHidden }) {
   const JISHO_URL = `http://jisho.org/search/${keyword}`;
 
   return (
-    <StyledAnchor visuallyHidden={visuallyHidden} href={JISHO_URL} external plainLink title="Search Jisho" tabindex="-1">
+    <StyledAnchor visuallyHidden={visuallyHidden} href={JISHO_URL} external plainLink title="Search Jisho" tabIndex="-1">
       <Icon name="WORD_SEARCH" viewBox="0 0 100 100" size="1.8em" />
     </StyledAnchor>
   );
