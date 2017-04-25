2a0473b43f2dbeb4a0570106662c2fb3c45651b3
Duncan Bay <duncan.bay@gmail.com>
2017-04-25 20:44:09 +0100

    refactor: cleanup BackgroundImg

--- app/components/BackgroundImg/index.js
+++ app/components/BackgroundImg/index.js
@@ -1,18 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const BackgroundImg = styled.div`
+/**
+ * Requires a parent with position: relative
+ */
+const StyledDiv = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  flex: 1 1 100%;
-  align-self: stretch;
-  background-image: url(${(props) => props.img});
+  background-image: url(${({ imgSrc }) => imgSrc});
   background-repeat: no-repeat;
   background-position: bottom right;
   background-size: cover;
   z-index: -1;
+  /* If parent is a flex container */
+  flex: 1 1 100%;
+  align-self: stretch;
 `;
+
+BackgroundImg.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+};
+
+function BackgroundImg({ imgSrc }) {
+  return (
+    <StyledDiv imgSrc={imgSrc} />
+  );
+}
 
 export default BackgroundImg;
