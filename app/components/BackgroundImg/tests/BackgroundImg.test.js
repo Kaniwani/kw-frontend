d735ecb437e7f0111fe36968b5840b54bfc23bd8
Duncan Bay <duncan.bay@gmail.com>
2017-04-27 13:22:44 +0100

    test: add description

--- app/components/BackgroundImg/tests/BackgroundImg.test.js
+++ app/components/BackgroundImg/tests/BackgroundImg.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BackgroundImg from '../index';
 
-it('should render with an imgSrc prop', () => {
-  const renderedComponent = shallow(
-    <BackgroundImg imgSrc={'http://fillmurray.com/400/800'} />
-  );
-  expect(renderedComponent).toMatchSnapshot();
+describe('<BackgroundImg />', () => {
+  it('should render with an imgSrc prop', () => {
+    const renderedComponent = shallow(
+      <BackgroundImg imgSrc={'http://fillmurray.com/400/800'} />
+    );
+    expect(renderedComponent).toMatchSnapshot();
+  });
 });
