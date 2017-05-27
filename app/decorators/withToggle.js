83f1ad3402ae220975c9a754785c7149b49b9781
Duncan Bay <duncan.bay@gmail.com>
2017-05-28 01:20:07 +0200

    refactor: move withTooltip, add withToggle - to decorators

---
+++ app/decorators/withToggle.js
@@ -0,0 +1,12 @@
+import { compose, withState, withHandlers } from 'recompose';
+
+const withToggle = compose(
+  withState('isToggled', 'updateToggle', false),
+  withHandlers({
+    toggleOn: ({ updateToggle }) => (/* event */) => updateToggle(true),
+    toggleOff: ({ updateToggle }) => (/* event */) => updateToggle(false),
+    toggle: ({ updateToggle }) => (/* event */) => updateToggle((currentValue) => !currentValue),
+  })
+);
+
+export default withToggle;
