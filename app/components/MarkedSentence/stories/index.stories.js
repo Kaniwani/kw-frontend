9cca70f025c05d93cc81a4726bb379caa09a54ae
Duncan Bay <duncan.bay@gmail.com>
2017-06-02 00:56:41 +0200

    fix: invalid story prop

--- app/components/MarkedSentence/stories/index.stories.js
+++ app/components/MarkedSentence/stories/index.stories.js
@@ -4,11 +4,11 @@ import MarkedSentence from '../index';
 
 storiesOf('components.MarkedSentence', module)
   .add('single MarkedSentence with no matches', () => (
-    <MarkedSentence sentence="その言葉の漢字は難しい" character="河豚" kana="ふぐ" />
+    <MarkedSentence sentence="その言葉の漢字は難しい" character="河豚" kana={['ふぐ']} />
   ))
   .add('single MarkedSentence with character match', () => (
-    <MarkedSentence sentence="その言葉の漢字は難しい" character="漢字" kana="かんじ" />
+    <MarkedSentence sentence="その言葉の漢字は難しい" character="漢字" kana={['かんじ']} />
   ))
   .add('single MarkedSentence with kana match', () => (
-    <MarkedSentence sentence="その言葉のかんじは難しい" character="漢字" kana="かんじ" />
+    <MarkedSentence sentence="その言葉のかんじは難しい" character="漢字" kana={['かんじ']} />
   ));
