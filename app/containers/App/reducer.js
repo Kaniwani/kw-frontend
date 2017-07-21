2c2441437464a099756fb84605f96c1aef8d5596
Duncan Bay <duncan.bay@gmail.com>
2017-07-21 15:32:28 +0200

    fix: current id accidentally wrapped in array

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -196,7 +196,7 @@ const reviewsReducer = handleActions({
     queue: { $set: union(state.queue, payload.reviewIds) },
   }),
   [app.reviews.current.set]: (state, { payload }) => update(state, {
-    current: { $set: 4037 },
+    current: { $set: payload },
     queue: { $set: difference(state.queue, [payload]) },
   }),
   [app.reviews.current.return]: (state, { payload }) => update(state, {
