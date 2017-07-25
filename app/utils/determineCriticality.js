e1fa4f0ea97ad393280581ce9456ffbf3e8c0aa6
Duncan Bay <duncan.bay@gmail.com>
2017-07-26 00:26:37 +0200

    feat: increase criticality minimum answered amount

--- app/utils/determineCriticality.js
+++ app/utils/determineCriticality.js
@@ -4,10 +4,10 @@
  * @param  {number} correct answered correctly count
  * @param  {number} incorrect answered incorrectly count
  * @param  {number} [threshold=0.75] Correctness ratio below which item is considered critical
- * @param  {number} [minimum=3] Number of times item must have been answered previously
+ * @param  {number} [minimum=4] Number of times item must have been answered previously
  * @return {boolean} True if item is below critical threshold
  */
-function determineCriticality(correct = 0, incorrect = 0, threshold = 0.75, minimum = 3) {
+function determineCriticality(correct = 0, incorrect = 0, threshold = 0.75, minimum = 4) {
   const answered = correct + incorrect;
   const correctRatio = correct / answered;
   return (answered >= minimum) && (correctRatio < threshold);
