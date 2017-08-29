c557739b59314381e38f529819e9839594e640bb
Duncan Bay <duncan.bay@gmail.com>
2017-08-29 15:54:27 +0800

    feat: about page content

--- app/containers/AboutPage/index.js
+++ app/containers/AboutPage/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import browserStackLogo from 'shared/assets/img/browserstack.svg';
 
 import PageWrapper from 'base/PageWrapper';
+import Container from 'base/Container';
+import Element from 'base/Element';
 import H1 from 'base/H1';
+import H2 from 'base/H2';
+import H3 from 'base/H3';
+import A from 'base/A';
+import P from 'base/P';
 
 function AboutPage() {
   return (
@@ -14,21 +19,85 @@ function AboutPage() {
         <meta name="description" content="Kaniwani About Page" />
       </Helmet>
       <PageWrapper>
-        <H1>Hello AboutPage</H1>
+        <Container>
+          <Element>
+            <H1 >What’s this all about then?</H1>
+            <P style={{ maxWidth: '760px' }}>KaniWani is a companion website to <A href="http://www.wanikani.com" external>WaniKani</A>, an SRS tool for kanji and vocabulary memorization. We felt that WaniKani helped a lot with being able to recognize and read Kanji, but didn’t help enough when trying to recall words without the visual stimulation of the Kanji. KaniWani attempts to bridge that gap by using your WaniKani vocabulary to force you to do the opposite of what you are used to.</P>
+          </Element>
+        </Container>
+
+        <Container flex flexRow flexWrap>
+          <Element flex="1 0 320px">
+            <H2 >Moving Forward</H2>
+            <P>KaniWani starts off at your current WK level, so you don’t have to go through all the previous levels unless you want to.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Unlocking the Past</H2>
+            <P>Manually <A to="/vocabulary">unlock or relock previous levels</A> or even individual words to manage the vocabulary in your review schedule.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Keyboard Warrior</H2>
+            <P>During reviews, press <strong>P</strong> to show the Phonetic reading, <strong>k</strong> to show the actual Kanji in the vocabulary, and <strong>f</strong> shows both <em>(a la WaniKani)</em>.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Speedy Gonzales</H2>
+            <P>Fly through your reviews by turning on the auto advance feature when you enter a correct answer, or show yourself all possible answers on failure.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Next Time Gadget</H2>
+            <P>When your answer has been marked incorrect, but you totally knew what it was, like, 本気で！ Press <strong>/</strong> on your keyboard or vehemently smack the <strong>x</strong> icon to ignore your failure.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Follow WaniKani</H2>
+            <P>You unlock new KaniWani vocab as you unlock new WaniKani vocab. Toggle features on the <A to="/settings">settings</A> page.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Vocab Synergy</H2>
+            <P>Words in WaniKani that have the same identical base meaning but different readings get combined. For example, when you see the word “Father” you can answer either “ちちおや” or “おとうさん” <em>(but not Anakin Skywalker)</em>.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Science Repeat... Science!</H2>
+            <P>The algorithm is identical to WaniKani’s SRS <em>(Spaced Repetition System)</em>, in that the more often you correctly answer a review, the less often you will see it — until it is burnt.</P>
+          </Element>
+          <Element flex="1 0 320px">
+            <H2 >Artisnal Artistry</H2>
+            <P>Feast your eyes on the colorful designs generously provided by <A href="mailto:mike_hoorn@hotmail.com?subject=mail from Kaniwani">Mike</A>. We think they’re the cutest 怪獣 this side of the 日本海.</P>
+          </Element>
+        </Container>
+
+        <Container flex flexColumn flexCenter>
+          <Element flex flexRow flexCenter>
+            <P textAlign="center">
+              <em>Contributions gratefully accepted for ongoing development, webhosting, and <span lang="ja">日本酒</span>.</em>
+            </P>
+          </Element>
+          <Element>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+              <input type="hidden" name="cmd" value="_s-xclick" />
+              <input
+                type="hidden"
+                name="encrypted"
+                value="-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAjC1eZQaGmWRtltMlBsnUpM9EGKTqYMqBIjMF4KELpQPI+uXpSJ4aU4zXM+N1YgyXUOD76vdLnQeNcB/qxzo5fgcwm0bFDWTVEh6rQDJDF3P/XmDFkmLKQYane0g/Q3NisIUBejSjHFwAtopkYExHM9pLWBUaI2cCTN28JfKyRBjELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIOUlYaYQLFs2AgYikFUt3W34e0PjEezuVTObwtoXz+EBCrsPlYd735Bj2nPNGpx2q4qkKhRIpeWC85rJVWutpySUXEGLqKOj6fo3nR3uARX/Hqc49eiA1Yev49O+dDERAxgw451dFER39hv3Em+PuyYUSqevLlcKulRzjPT7N959FPq7i01QDmScj0m05JLJUTHfgoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTYwMjA4MjAyNTM1WjAjBgkqhkiG9w0BCQQxFgQUD6bc9NoP3NotvwDEQSjqFitkXGwwDQYJKoZIhvcNAQEBBQAEgYCpvXhxpQn6MBq/dird9lFYvWleBBcBJuecAKYr9gMiB7JHmhMwb5T/9MgyFMH5eUttcdmLx9AXA51uLMIrN2ovvRiSMhTMprScE0xR9okzlu5BnKofOuhTlmR7DijTgThk8JyWQOYn/BPmOdkbN8gukBJGaDcKid1qM269ZfYz5Q==-----END PKCS7-----"
+              />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+              <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </Element>
+        </Container>
+        <Container flex flexColumn flexCenter>
+          <Element>
+            <H3>Browser Testing generously provided by</H3>
+          </Element>
+          <Element style={{ height: '3.5rem' }}>
+            <A plainLink href="https://www.browserstack.com/" external>
+              <img style={{ width: '100%', height: '100%' }} alt="BrowserStack Logo" src={browserStackLogo} />
+            </A>
+          </Element>
+        </Container>
+
       </PageWrapper>
     </div>
   );
 }
 
-AboutPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-};
-
-
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-export default connect(null, mapDispatchToProps)(AboutPage);
+export default AboutPage;
