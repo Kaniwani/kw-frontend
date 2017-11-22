535aa820760222806d5c12a464d828616dd01417
Duncan Bay <duncan.bay@gmail.com>
2017-11-22 22:10:37 +1100

    refactor(UpcomingReviewsChart): remove day reference line
    line overlaps count when a bar aligns on the day start, making it hard to read both

--- app/components/UpcomingReviewsChart/DayTick.js
+++ app/components/UpcomingReviewsChart/DayTick.js
@@ -1,37 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { greyDark, grey } from 'shared/styles/colors';
+import { greyDark } from 'shared/styles/colors';
 
 const DayTick = ({
   x, y, width, height, payload: { value },
-}) => value ? (
-  <g>
-    <text
-      width={width}
-      height={height}
-      x={x}
-      y={y}
-      dx={0}
-      dy={-10}
-      textAnchor="middle"
-      fill={greyDark}
-      fontSize=".85rem"
-    >
-      {value}
-    </text>
-    {/* manually render a reference line */}
-    <line
-      x1={x}
-      y1={y - 6}
-      x2={x}
-      y2={y + 2}
-      stroke={grey}
-      strokeWidth={1.5}
-      fill="none"
-    />
-  </g>
-) : null;
+}) =>
+  value ? (
+    <g>
+      <text
+        width={width}
+        height={height}
+        x={x}
+        y={y}
+        dx={0}
+        dy={-10}
+        textAnchor="middle"
+        fill={greyDark}
+        fontSize=".85rem"
+      >
+        {value}
+      </text>
+    </g>
+  ) : null;
 
 /* eslint-disable react/require-default-props */
 DayTick.propTypes = {
