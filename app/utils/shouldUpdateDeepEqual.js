5b8d054112fbb76bc136e5160f2c6c811ba91736
Duncan Bay <duncan.bay@gmail.com>
2017-11-27 19:08:15 +1100

    fix: ensure date is parsed

--- app/utils/shouldUpdateDeepEqual.js
+++ app/utils/shouldUpdateDeepEqual.js
@@ -2,14 +2,14 @@ import { isEqual } from 'lodash';
 import { shouldUpdate } from 'recompose';
 
 /**
- * HoC which applies shouldUpdate comparing deep equal for provided prop names, if none given tests every prop
+ * HoC which applies shouldUpdate comparing deep equal for provided prop names, else tests every prop
  * @param  {Array} propNames props to test if deep equal
  * @return {Function} HoC expecting a component
  */
 const shouldUpdateDeepEqual = (propNames) =>
   shouldUpdate((props, nextProps) =>
-    Array.isArray(propNames) ?
-      propNames.some((propName) => !isEqual(props[propName], nextProps[propName])) :
-      !isEqual(props, nextProps));
+    Array.isArray(propNames)
+      ? propNames.some((propName) => !isEqual(props[propName], nextProps[propName]))
+      : !isEqual(props, nextProps));
 
 export default shouldUpdateDeepEqual;
