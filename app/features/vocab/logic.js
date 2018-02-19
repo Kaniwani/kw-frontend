d417a5b76f1df3c6d71a1cb4d25cc5b5e332bc61
Duncan Bay <duncan.bay@gmail.com>
2018-02-19 12:44:26 +1100

    fix: report logic action dispatching

--- app/features/vocab/logic.js
+++ app/features/vocab/logic.js
@@ -5,8 +5,16 @@ import vocab from './actions';
 const reportVocabLogic = createLogic({
   type: vocab.report.request,
   process: ({ api, action }, dispatch, done) => {
-    dispatch(api.report.send(action.payload));
-    done();
+    api.report
+      .send(action.payload)
+      .then(() => {
+        dispatch(vocab.report.success());
+        done();
+      })
+      .catch((err) => {
+        dispatch(vocab.report.failure(err));
+        done();
+      });
   },
 });
 
