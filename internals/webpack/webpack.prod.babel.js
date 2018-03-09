ae601bf55c9818670e3c916692ce1f6d02ce717a
Duncan Bay <duncan.bay@gmail.com>
2018-03-09 13:20:37 +1100

    chore: update offline-plugin options  - delay video file loading

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -67,17 +67,13 @@ module.exports = require('./webpack.base.babel')({
         events: true,
       },
 
-      // No need to cache .htaccess. See http://mxs.is/googmp,
-      // this is applied before any match in `caches` section
-      excludes: ['.htaccess'],
-
       caches: {
         main: [':rest:'],
 
         // All chunks marked as `additional`, loaded after main section
         // and do not prevent SW to install. Change to `optional` if
         // do not want them to be preloaded at all (cached only when first loaded)
-        additional: ['*.chunk.js', '*.png', '*.svg', '*.jpg'],
+        additional: ['*.chunk.js', '*.mp4', '*.webm', '*.png', '*.svg', '*.jpg', '*.woff*'],
       },
 
       // Removes warning for about `additional` section usage
