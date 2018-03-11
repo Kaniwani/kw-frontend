1172ae17812ea7cd7d7e82cfd8b6df6871e10c85
Duncan Bay <duncan.bay@gmail.com>
2018-03-11 19:51:30 +1100

    feat: replace user answer with matched word instead of just reading

--- app/features/quiz/QuizSession/QuizAnswer/utils.js
+++ app/features/quiz/QuizSession/QuizAnswer/utils.js
@@ -1,30 +1,40 @@
 import { isJapanese } from 'wanakana';
 import { endsWith } from 'voca';
-import { flatMap } from 'lodash';
 import stripTilde from 'common/utils/stripTilde';
 
 export const increment = (x = 0) => x + 1;
 export const decrement = (x = 0) => Math.max(0, x - 1);
-export const containsZenKaku = (input = '') => /[ａ-ｚ]/gi.test(input);
-export const isInputValid = (input = '') => !containsZenKaku(input) && isJapanese(input, /[0-9]/);
+export const containsZenkakuLatin = (input = '') => /[ａ-ｚ]/gi.test(input);
+export const isInputValid = (input = '') =>
+  !containsZenkakuLatin(input) && isJapanese(input, /[0-9]/);
 export const cleanseInput = (input = '') => fixTerminalN(input.trim());
 
-const cleanAnswer = (text) => ({
-  original: text,
-  cleaned: stripTilde(text),
-});
+function flattenReadings([vocab = [], synonyms = []] = []) {
+  return [
+    ...vocab.map(({ word, primaryReading, secondaryReadings = [] } = {}) => ({
+      word,
+      readings: [primaryReading, ...secondaryReadings],
+    })),
+    ...synonyms.map(({ word, primaryReading } = {}) => ({
+      word,
+      readings: [primaryReading],
+    })),
+  ];
+}
 
 /**
  * Compares input to answers - ignoring any tildes
  * @param  {String} [input=""] user input
- * @param  {[String]} [answers=[]] allowed answers
+ * @param  {[Object]} [answers=[]] allowed answer vocab/synonym items
  * @return {String} matched answer (incl. relevant tildes) or ""
  */
 export function matchAnswer(input = '', answers = []) {
   const cleanedInput = stripTilde(input);
-  const cleanedAnswers = answers.map(cleanAnswer);
-  const match = cleanedAnswers.find(({ cleaned }) => cleaned === cleanedInput);
-  return match ? match.original : '';
+  const re = new RegExp(`[〜~]?${cleanedInput}[〜~]?`);
+  return flattenReadings(answers).reduce(
+    (ret, { word, readings }) => ([...readings, word].some((vocab) => re.test(vocab)) ? word : ret),
+    ''
+  );
 }
 
 /**
@@ -37,22 +47,3 @@ export function fixTerminalN(input = '') {
   const ja = 'ｎ';
   return endsWith(input, en) || endsWith(input, ja) ? `${input.slice(0, -1)}ん` : input;
 }
-
-/**
- * Combines vocab and synonym objects into a flat list of possible answers
- * @param  {Array} [vocab=[]] - vocab entries
- * @param  {Array} [synonyms=[]] - synonym entries
- * @return {[String]} combined answers
- */
-export function combineAnswers(vocab = [], synonyms = []) {
-  return [
-    ...flatMap(vocab, ({ word, primaryReading, secondaryReadings = [] } = {}) => [
-      word,
-      primaryReading,
-      ...secondaryReadings,
-    ]),
-    ...flatMap(synonyms, ({ word, primaryReading } = {}) => [word, primaryReading]),
-  ].filter(Boolean); // remove "" || undefined
-}
-
-export default combineAnswers;
