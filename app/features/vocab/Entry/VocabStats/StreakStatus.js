b39af6d3ae6b966684610863a06a494586e1408b
Duncan Bay <duncan.bay@gmail.com>
2018-03-15 18:34:18 +1100

    feat: use srs colors in vocab detail streak icons

--- app/features/vocab/Entry/VocabStats/StreakStatus.js
+++ app/features/vocab/Entry/VocabStats/StreakStatus.js
@@ -4,17 +4,16 @@ import styled from 'styled-components';
 
 import getSrsRankName from 'common/utils/getSrsRankName';
 import StreakIcon from 'common/components/StreakIcon';
-
+import { SRS_COLORS } from 'common/styles/colors';
 import { gutter } from 'common/styles/layout';
 import H5 from 'common/components/H5';
 
 const Wrapper = styled.div`
   display: inline-flex;
   align-items: center;
-  ${gutter({ type: 'outer', position: 'right' })}
+  ${gutter({ type: 'outer', position: 'right' })};
 `;
-const Text = H5.extend`
-`;
+const Text = H5.extend``;
 
 StreakStatus.propTypes = {
   streak: PropTypes.number.isRequired,
@@ -22,13 +21,13 @@ StreakStatus.propTypes = {
 };
 
 function StreakStatus({ streak, category }) {
+  const name = getSrsRankName(streak);
   return (
     <Wrapper>
       <Text>{category}: </Text>
-      <StreakIcon streakName={getSrsRankName(streak)} />
+      <StreakIcon streakName={name} color={SRS_COLORS[name]} />
     </Wrapper>
   );
 }
-
 
 export default StreakStatus;
