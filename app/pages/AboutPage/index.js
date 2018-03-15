3e283114829f2034868b09de7e62eeb77c660228
Duncan Bay <duncan.bay@gmail.com>
2018-03-15 14:56:55 +1100

    feat: update about page text content, minor layout adjustments

--- app/pages/AboutPage/index.js
+++ app/pages/AboutPage/index.js
@@ -26,11 +26,9 @@ function AboutPage() {
         <meta name="description" content="Kaniwani About Page" />
       </Helmet>
       <PageWrapper>
-        <Container>
-          <Element>
+        <Container flexRow flexWrap>
+          <Container flex="10 0 100%">
             <H1>What’s this all about then?</H1>
-          </Element>
-          <Element>
             <P constrain>
               KaniWani is a companion website to{' '}
               <A href="http://www.wanikani.com" external>
@@ -41,11 +39,9 @@ function AboutPage() {
               bridge that gap by using your WaniKani vocabulary to force you to do the opposite of
               what you are used to.
             </P>
-          </Element>
-        </Container>
+          </Container>
 
-        <Container flexRow flexWrap>
-          <Element flex="1 0 320px">
+          <Container flex="1 1 500px">
             <H2>Moving Forward</H2>
             <P constrain>
               KaniWani starts off at your current WaniKani level, so you don’t have to go through
@@ -53,16 +49,40 @@ function AboutPage() {
               your lesson queue. Complete lessons to introduce words into your regular review
               schedule.
             </P>
-          </Element>
-          <Element flex="1 0 320px">
+          </Container>
+          <Container flex="1 1 500px">
             <H2>Unlocking the Past</H2>
             <P constrain>
               Manually unlock and relock previous levels in the vocabulary section, or even
-              individual words to filter the vocabulary in your review schedule. You can further
-              filter your reviews by WaniKani SRS rank on the settings page.
+              individual words to filter the vocabulary in your review schedule. Locking a level
+              completely resets the (KaniWani) SRS rank for all the contained words. Individually
+              disabling a word will filter it from your reviews and you can re-enable it later to
+              resume progress. You can also further filter your reviews by WaniKani SRS rank on the
+              settings page.
             </P>
-          </Element>
-          <Element flex="1 0 320px">
+          </Container>
+          <Container flex="1 1 500px">
+            <H2>Follow WaniKani</H2>
+            <P constrain>
+              This setting will automatically sync with WaniKani and unlock <strong>new</strong>{' '}
+              levels here as you level up there.{' '}
+              <strong>
+                If you intend to cancel or suspend your WaniKani account, you should turn this
+                feature off beforehand
+              </strong>{' '}
+              to prevent future syncing when your WaniKani level resets to level 3!.
+            </P>
+          </Container>
+
+          <Container flex="1 1 500px">
+            <H2>Science Repeat... Science!</H2>
+            <P constrain>
+              The algorithm is the same as WaniKani’s SRS, in that the more often you correctly
+              answer a review, the less often you will see it — until it is burnt.
+            </P>
+          </Container>
+
+          <Container flex="1 1 500px">
             <H2>Keyboard Warrior</H2>
             <P constrain>
               After answering a question, press <Shortcut>f</Shortcut> to open info panel,{' '}
@@ -70,8 +90,8 @@ function AboutPage() {
               <Shortcut>w</Shortcut> to enable wrap-up mode, and <Shortcut>s</Shortcut> to add a new
               answer synonym.
             </P>
-          </Element>
-          <Element flex="1 0 320px">
+          </Container>
+          <Container flex="1 1 500px">
             <H2>Next Time Gadget</H2>
             <P constrain>
               If your answer is marked incorrect, but you totally knew what it was, truly your
@@ -84,35 +104,16 @@ function AboutPage() {
               <strong> mark your correct answer as incorrect and penalise appropriately</strong>.
               Use with caution!
             </P>
-          </Element>
-          <Element flex="1 0 320px">
-            <H2>Follow WaniKani</H2>
+          </Container>
+          <Container flex="1 1 500px">
+            <H2>Girl, Little Girl, Young Lady, Arggh!</H2>
             <P constrain>
-              Automatically sync with WaniKani and unlock new levels here as you level up there.{' '}
-              <strong>
-                If you intend to cancel or suspend your WaniKani account, you should turn this
-                feature off beforehand
-              </strong>{' '}
-              to prevent future syncing when your WaniKani level resets to level 3!.
+              Many WaniKani words have very similar meanings. To alleviate this, if you add{' '}
+              <strong>meaning synoynms on WaniKani</strong> they will appear as part of the KaniWani
+              meaning. You can also add <strong>answer synonyms on KaniWani</strong> as acceptable
+              answers during your reviews or on the vocabulary entry page.
             </P>
-          </Element>
-          <Element flex="1 0 320px">
-            <H2>Science Repeat... Science!</H2>
-            <P constrain>
-              The algorithm is the same as WaniKani’s SRS, in that the more often you correctly
-              answer a review, the less often you will see it — until it is burnt.
-            </P>
-          </Element>
-          <Element flex="1 0 320px">
-            <H2>Vocab Synergy</H2>
-            <P constrain>
-              Words in WaniKani that have the same <strong>identical</strong> base meaning but
-              different readings get combined. For example, when you see the word “Arrow” you can
-              answer either “や” or “やじるし”. If the meanings are slightly different but you
-              consider your answer valid - you can add it as a KaniWani answer synonym during
-              reviews or on the item’s vocabulary page.
-            </P>
-          </Element>
+          </Container>
         </Container>
 
         <Container flexColumn flexCenter>
