d57ba5546c6b407937978511c29ed93157739f3a
Duncan Bay <duncan.bay@gmail.com>
2018-04-28 18:59:21 +1000

    fix: jest failing to transform es6
    Suddenly no longer working, have explicitly added babel-jest which was somehow
    missing from dependencies?

--- internals/testing/setupTests.js
+++ internals/testing/setupTests.js
@@ -1,15 +1,5 @@
-/* eslint-disable import/first */
-
-// Temporary hack to suppress react RAF test error
-// https://github.com/facebookincubator/create-react-app/issues/3199
-import "raf/polyfill";
-window.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
-  return 0;
-};
-
 // enzyme setup
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() });
