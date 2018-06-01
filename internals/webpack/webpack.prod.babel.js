827f491bb9f5b7565eaca9ce397eac08c8785f9e
Duncan Bay <duncan.bay@gmail.com>
2018-06-01 11:12:08 +1000

    feat: fullscreen display for installed app

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -50,7 +50,7 @@ module.exports = require('./webpack.base.babel')({
       short_name: 'KaniWani',
       background_color: '#fafafa',
       theme_color: '#8523e7',
-      display: 'minimal-ui',
+      display: 'fullscreen',
       orientation: 'portrait',
       start_url: '/',
       ios: true,
