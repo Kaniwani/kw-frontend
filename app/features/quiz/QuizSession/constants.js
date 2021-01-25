1403f26f210dea7f863f19ef9ed4cb022921d925
Duncan Bay <duncan.bay@gmail.com>
2021-01-25 13:58:39 +1100

    feat: increase queue sizes for better random sampling across levels

--- app/features/quiz/QuizSession/constants.js
+++ app/features/quiz/QuizSession/constants.js
@@ -1,5 +1,5 @@
-export const INITIAL_QUEUE_LIMIT = 50;
-export const SUBSEQUENT_QUEUE_LIMIT = 20;
+export const INITIAL_QUEUE_LIMIT = 100;
+export const SUBSEQUENT_QUEUE_LIMIT = 50;
 export const MINIMUM_QUEUE_COUNT = 3;
 export const WRAP_UP_STARTING_COUNT = 10;
 export const SESSION_CATEGORIES = {
